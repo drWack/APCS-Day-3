f8f3aca93f30b3a9aca9223a0b8627d3445ef8e0
Jacob Machado <34262270+drWack@users.noreply.github.com>
2021-08-04 17:46:23 +0000

    10:46 finished randomstringchooser

--- Main.java
+++ Main.java
@@ -1,12 +1,20 @@
+import java.util.*; //arraylist,collection,random
+
 class Main {
   public static void main(String[] args) {
 
-    System.out.println(fibonacci(5));
+    String[] words = {"aint ", "gon", "happen","white","purple"};
+    randomStringChooser myChooser = new randomStringChooser(words);
+    
+    for(int k =0; k <6; k++){
+      System.out.println(myChooser.getNext());
+    }
+
   }
   
   
-  // 1 1 2 3 5 8 13 21 34 55 
   static int fibonacci(int n){
+    // 1 1 2 3 5 8 13 21 34 55 
     if(n<= 1){
       return n;
     }
@@ -14,5 +22,33 @@ class Main {
     System.out.println(sum);
     return sum;
   }
+}
+class randomStringChooser{ 
+
+  private ArrayList<String> wordList;
+
+  public randomStringChooser(String[] wordArray){
+
+    wordList = new ArrayList<String>();
+    
+    for(String word : wordArray){
+      wordList.add(word);
+    }
+  }
+  public String getNext(){
+    Random myrand = new Random();
+
+      if(wordList.size() <= 0){
+        return "NONE";
+      }
+      else{
+        int maxsize = wordList.size();
+        double randDub = myrand.nextDouble();
+        int indexToRemove = (int)(maxsize*randDub);
+
+        return wordList.remove(indexToRemove);
+      }
+  }
+
 
 }
