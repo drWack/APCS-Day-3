1dfd75a29f7c11acc9bcaeec3dcd6dd843501c9c
Jacob Machado <34262270+drWack@users.noreply.github.com>
2021-08-04 17:16:09 +0000

    Added Fibonacci 10:16

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    System.out.println(fibonacci(5));
   }
+  
+  
+  // 1 1 2 3 5 8 13 21 34 55 
+  static int fibonacci(int n){
+    if(n<= 1){
+      return n;
+    }
+    int sum = fibonacci(n-1) + fibonacci(n-2);
+    System.out.println(sum);
+    return sum;
+  }
+
 }
